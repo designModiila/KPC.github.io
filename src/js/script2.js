4a1ee7243004423268c5cb917bc3ee3b52f70e54
designModiila <stpj0005@sorin.co.kr>
2024-01-03 10:39:19 +0900

    스크립트수정

--- src/js/script2.js
+++ src/js/script2.js
@@ -8,7 +8,6 @@ $(function() {
     css3:true,
     scrollOverflow: true,
     responsiveWidth: 1023,
-    responsiveHeight: 760,
     onLeave: function(anchorLink, index, direction) {
       $.fn.fullpage.setAllowScrolling(true);
       if(index == 1){
